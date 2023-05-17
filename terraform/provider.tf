90c63144f5a74eaf5f80263673036184ee78872c
awkto <awkto@outlook.com>
2023-05-17 20:48:23 +1000

    updated variable names

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,9 +7,16 @@ terraform {
   }
 }
 
+#related to github actions secrets
+provider "digitalocean" {
+  token = var.digitalocean_token
+}
+
+#previously related to environment variables
 variable "do_token" {}
 #variable "pvt_key" {}
 
+#previously related to environment variables
 provider "digitalocean" {
   token = var.do_token
 }
